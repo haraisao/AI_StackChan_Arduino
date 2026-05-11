286aff5ee502f1f352ffce1adc7b968a520b8657
Isao Hara <haraisao@gmail.com>
2026-05-11 14:56:45 +0900

    Update Motion for RT_VERSION

--- src/motion/Motion.cpp
+++ src/motion/Motion.cpp
@@ -10,12 +10,21 @@ void refuseMotion(StackchanSERVO *servo) {
   if (!servo) return;
   if (isMoving) return;
   isMoving=1;
+#ifdef RT_VERSION
+  servo->moveXY(180, 90, 500);
+  servo->moveX(170, 200);
+  servo->moveX(190, 400);
+  servo->moveX(170, 400);
+  servo->moveX(190, 400);
+  servo->moveXY(180, 90, 200);
+#else
   servo->moveXY(90, 90, 500);
   servo->moveX(80, 200);
   servo->moveX(100, 400);
   servo->moveX(80, 400);
   servo->moveX(100, 400);
   servo->moveXY(90, 90, 200);
+#endif
   delay(500);
   isMoving=0;
 }
@@ -24,9 +33,16 @@ void nodMotion(StackchanSERVO *servo, int count) {
   if (!servo) return;
   if (isMoving) return;
   isMoving=1;
-#if 1
+#ifdef RT_VERSION
+  servo->moveXY(180, 90, 500);
+  for(int i=0; i< count; i++){
+    servo->moveY(80, 400);
+    servo->moveY(100, 800);
+    delay(100);
+  }
+  servo->moveXY(180, 90, 500);
+#else
   servo->moveXY(90, 90, 500);
-
   for(int i=0; i< count; i++){
     servo->moveY(80, 400);
     servo->moveY(100, 800);
@@ -52,6 +68,11 @@ void upMotion(StackchanSERVO *servo, int sigV, int sigH) {
 
 void myMotion(StackchanSERVO *servo, int m) {
   if (!servo) return;
+#ifdef RT_VERSION
+  int flag = -1;
+#else
+  int flag = 1;
+#endif
   switch(m){
     case 0:
       refuseMotion(servo);
@@ -60,16 +81,16 @@ void myMotion(StackchanSERVO *servo, int m) {
       nodMotion(servo);
       break;
     case 2:
-      upMotion(servo, -1, -1);
+      upMotion(servo, -1, -1*flag);
       break;
     case 3:
-      upMotion(servo, 1, -1);
+      upMotion(servo, 1, -1*flag);
       break;
     case 4:
-      upMotion(servo, -1, 1);
+      upMotion(servo, -1, 1*flag);
       break;
     case 5:
-      upMotion(servo, 1, 1);
+      upMotion(servo, 1, 1*flag);
       break;
     default:
       servo->moveXY(90,90, 500);
